5cc0854744b306a0a4f17f2243594068875c1c28
odoric27 <dmo294@nyu.edu>
2017-03-05 15:56:11 -0500

    resolving more conflicts plus...
    there were some whitespace errors that needed to be resolved.
    also - the validation of expiration date would throw 500 server error when passed bad data. fixed above so that if mm/YYYY can't convert to datetime object then ValueError is caught and handled, resulting in is_valid returning False.

--- payments.py
+++ payments.py
@@ -82,16 +82,15 @@ def add_payment():
 	if data is None:
 		return make_response(CONTENT_ERR_MSG, HTTP_400_BAD_REQUEST)
 
-	try:
-    is_valid(data):
+	if is_valid(data):
 		id = index_inc()
 		newData = {'id' : id, 'default' : False, 'nickname' : data['nickname'],
-				   'type' : data['type'], 'detail' : data['detail']}
+			   'charge-history' : 0.0, 'type' : data['type'], 'detail' : data['detail']}
 		payments.append(newData)
 		message = {'successfully created' : payments[id-1]}
 		rc = HTTP_201_CREATED
-	except KeyError as err:
-		message = {'error' : 'Missing parameter error: %s' % err }
+	else:
+		message = {'error' : 'Data is not valid.' }
 		rc = HTTP_400_BAD_REQUEST
 	
 	response = make_response(jsonify(message), rc)
@@ -223,38 +222,40 @@ def charge_payment():
 #   U T I L I T I E S
 ######################################################################
 def index_inc():
-    global current_payment_id
-    with lock:
-        current_payment_id += 1
-    return current_payment_id
+	global current_payment_id
+	with lock:
+		current_payment_id += 1
+	return current_payment_id
 
 def is_valid(data):
-    valid = False
-    valid_detail = False
-    try:
-        nickname = data['nickname']
-        type = data['type']
-        detail = data['detail']
-	
-	if bool(re.search(r'\d', detail['name'])) == False:
-		valid = True
+	valid = False
+	valid_detail = False
+	try:
+		nickname = data['nickname']
+		type = data['type']
+		detail = data['detail']
 
-	if type == 'credit' or type == 'debit':
-		name = detail['name']
-		card_number = detail['number']
-		expires_date = detail['expires']
-		subtype = detail['type']
-		if bool(re.match('^[0-9]+$', card_number)) == True and len(card_number) == 16 and time.strptime(expires_date, "%m/%Y") != ():
+		if bool(re.search(r'\d', detail['name'])) == False:
+			valid = True
+
+		if type == 'credit' or type == 'debit':
+			name = detail['name']
+			card_number = detail['number']
+			expires_date = detail['expires']
+			subtype = detail['type']
+
+		if bool(re.match('^[0-9]+$', card_number)) and (len(card_number) == 16):
+			datetime.strptime(expires_date, '%m/%Y')
 			valid_detail = True
-	else:
-		valid_detail = True
-		
-    except KeyError as err:
-        app.logger.warn('Missing parameter error: %s', err)
-    except TypeError:
-        app.logger.warn('Invalid Content Type error')
 
-    return valid & valid_detail
+	except KeyError as err:
+		app.logger.warn('Missing parameter error: %s', err)
+	except TypeError:
+		app.logger.warn('Invalid Content Type error')
+	except ValueError:
+		app.logger.warn('Invalid Content Type error')
+
+	return valid & valid_detail
 
 def is_expired(payment):
 	#get datetime object for last day of expiring month
