2f847ec5849f9c2897d46349405415e5e0370afe
odoric27 <dmo294@nyu.edu>
2017-03-29 15:56:56 -0400

    ok... i think this is the one. there's a single 'Payment' object with an associated 'Detail' object that holds the payment-specific info (paypal vs credit vs debit). A quick playthrough with sqlite seemed to go okay with this model, but i'm writing proper tests now. stand by

--- models.py
+++ models.py
@@ -11,10 +11,9 @@ class DataValidationError(ValueError):
 # Models
 ######################################################################
 
-'''
-class BasePayment(db.Model):
-    __tablename__ = 'payments'
-    payment_id = db.Column(db.Integer, primary_key=True)
+class Payment(db.Model):
+    __tablename__ = 'payment'
+    id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(20))
     user_id = db.Column(db.Integer)
     payment_type = db.Column(db.String(10))
@@ -22,28 +21,92 @@ class BasePayment(db.Model):
     is_removed = db.Column(db.Boolean)
     charge_history = db.Column(db.Float)
 
-    __mapper_args__ = {
-        'polymorphic_identity':'payments',
-        'polymorphic_on': payment_type,
-        'with_polymorphic':'*'
-    }
+    detail_id = db.Column(db.Integer, db.ForeignKey('detail.id'))
+    detail = db.relationship('Detail',
+        backref=db.backref('payment', lazy='joined'))
 
     def __init__(self):
         self.is_default = False;
         self.is_removed = False;
         self.charge_history = 0.0
 
-
     def self_url(self):
         return url_for('get_payments', id=self.id, _external=True)
 
     def serialize(self):
-        pass
+        return {
+                    'payment_id' : self.payment_id,
+                    'user_id' : self.user_id,
+                    'nickname' : self.nickname,
+                    'payment_type' : self.payment_type,
+                    'is_default' : self.is_default,
+                    'charge_history' : self.charge_history,
+                    'details' : self.details.serialize()
+                }
 
     def deserialize(self, data):
-        pass
-'''
+        try:
+            self.user_id = data['user_id']
+            self.nickname = data['nickname']
+            self.payment_type = data['payment_type']
+            self.is_default = False
+            self.is_removed = False
+            self.charge_history = 0.0
+            details = data['details']
+            d = Detail()
+            if (self.payment_type == 'credit' or self.payment_type == 'debit'):
+                self.details = d.deserialize_card(details)
+            else:
+                self.details = d.deserialize_paypal(details)
+        except KeyError as e:
+            raise DataValidationError('Invalid payment: missing ' + e.args[0])
+        except TypeError as e:
+            raise DataValidationError('Invalid payment: body of request contained bad or no data')
+        return self
 
+class Detail(db.Model):
+    __tablename__ = 'detail'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50))
+    expires = db.Column(db.String(7))
+    card_type = db.Column(db.String(10))
+    card_number = db.Column(db.String(16))
+    user_email = db.Column(db.String(50))
+    is_linked = db.Column(db.Boolean)
+
+    def serialize(self):
+        result = {}
+        for key in self.__dict__:
+            if not self.__dict__[key] is None:
+                result[key] = self.__dict__[key]
+        return result
+
+
+    def deserialize_card(self, details):
+        try:
+            self.user_name = details['user_name']
+            self.card_type = details['card_type']
+            self.card_number = details['card_number']
+            self.expires = details['expires']
+        except KeyError as e:
+            raise DataValidationError('Invalid payment: missing ' + e.args[0])
+        except TypeError as e:
+            raise DataValidationError('Invalid payment: body of request contained bad or no data')
+        return self
+
+    def deserialize_paypal(self, details):
+        try:
+            self.user_name = details['user_name']
+            self.user_email = details['user_email']
+            self.is_linked = True
+        except KeyError as e:
+            raise DataValidationError('Invalid payment: missing ' + e.args[0])
+        except TypeError as e:
+            raise DataValidationError('Invalid payment: body of request contained bad or no data')
+        return self
+
+
+'''
 class CreditPayment(db.Model):
     __tablename__ = 'credit-cards'
     payment_id = db.Column(db.Integer, primary_key=True)
@@ -142,7 +205,7 @@ class DebitPayment(db.Model):
             raise DataValidationError('Invalid payment: body of request contained bad or no data')
         return self
 
-class PaypalPayment():
+class PaypalPayment(db.Model):
     __tablename__ = 'paypal-accounts'
     payment_id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(20))
@@ -187,3 +250,4 @@ class PaypalPayment():
         except TypeError as e:
             raise DataValidationError('Invalid payment: body of request contained bad or no data')
         return self
+'''
