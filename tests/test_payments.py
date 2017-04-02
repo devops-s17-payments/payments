9c92cf39a69bbc6a078a8ffd7fcd8ea8f95b2678
Jesse Gumz <jdg513@nyu.edu>
2017-04-02 18:29:49 -0400

    fix tests

--- tests/test_payments.py
+++ tests/test_payments.py
@@ -4,9 +4,8 @@
 
 import unittest
 
-import db
 from app import payments
-from db import db, models
+from db import app_db, models
 
 
 class TestModels(unittest.TestCase):
@@ -14,8 +13,8 @@ class TestModels(unittest.TestCase):
     def setUp(self):
         payments.app.debug = True
         payments.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://payments:payments@localhost:5432/test'
-        db.drop_all()    # clean up the last tests
-        db.create_all()  # make our sqlalchemy tables
+        app_db.drop_all()    # clean up the last tests
+        app_db.create_all()  # make our sqlalchemy tables
         
         data = {'nickname' : 'my credit', 'user_id' : 1, 'payment_type' : 'credit', 
         'details' : {'user_name' : 'Jimmy Jones', 'card_number' : '1111222233334444',
@@ -23,18 +22,18 @@ class TestModels(unittest.TestCase):
 
         payment = models.Payment()
         payment.deserialize(data)
-        db.session.add(payment)
-        db.session.commit()
+        app_db.session.add(payment)
+        app_db.session.commit()
         self.app = payments.app.test_client()
 
     def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+        app_db.session.remove()
+        app_db.drop_all()
 
     def test_db_has_one_item(self):
-        p1 = db.session.query(models.Payment).get(1)
+        p1 = app_db.session.query(models.Payment).get(1)
         self.assertNotEqual(p1, None)
-        p2 = db.session.query(models.Payment).get(2)
+        p2 = app_db.session.query(models.Payment).get(2)
         self.assertEqual(p2, None)
 
     def test_credit_has_no_paypal_fields(self):
