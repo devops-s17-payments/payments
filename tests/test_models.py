8ee48a17ada50181f7639cfbc85caf27e54dd595
odoric27 <dmo294@nyu.edu>
2017-04-03 02:10:49 -0400

    added tests for serialize, self_url. fixed field name error in models ('payment_id' should be 'id')

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,27 +3,27 @@
 # nosetests -v --rednose --nologcapture
 
 import unittest
-
+#from mock import patch
 from app import payments
 from app.db import app_db
 from app.db.models import Payment, Detail
 
-class TestModels(unittest.TestCase):
+CREDIT_DETAIL = {'user_name' : 'John Jameson', 'card_number' : '4444333322221111',
+                 'expires' : '02/2020', 'card_type' : 'Visa'}
 
-CREDIT = {'nickname' : 'my credit', 'user_id' : 1, 'payment_type' : 'credit', 
-          'details' : {'user_name' : 'Jimmy Jones', 'card_number' : '1111222233334444',
-          'expires' : '01/2019', 'card_type' : 'Mastercard'}}
+PAYPAL_DETAIL = {'user_name' : 'John Jameson', 'user_email' : 'jj@aol.com'}
 
-DEBIT = {'nickname' : 'my debit', 'user_id' : 2, 'payment_type' : 'debit', 
-         'details' : {'user_name' : 'John Jameson', 'card_number' : '4444333322221111',
-         'expires' : '02/2020', 'card_type' : 'Visa'}}
+CREDIT = {'nickname' : 'my credit', 'user_id' : 1,
+          'payment_type' : 'credit', 'details' : CREDIT_DETAIL}
 
-PAYPAL = {'nickname' : 'my paypal', 'user_id' : 2, 'payment_type' : 'paypal', 
-          'details' : {'user_name' : 'John Jameson', 'user_email' : 'jj@aol.com'}}
+PAYPAL = {'nickname' : 'my paypal', 'user_id' : 2,
+          'payment_type' : 'paypal', 'details' : PAYPAL_DETAIL}
 
 #note 'nickname' is spelled wrong
-BAD_DATA = {'nicknam3' : 'my paypal', 'user_id' : 2, 'payment_type' : 'paypal', 
-            'details' : {'user_name' : 'John Jameson', 'user_email' : 'jj@aol.com'}}
+BAD_DATA = {'nicknam3' : 'bad', 'user_id' : 2,
+            'payment_type' : 'paypal', 'details' : PAYPAL_DETAIL}
+
+class TestModels(unittest.TestCase):
 
     def setUp(self):
         payments.app.debug = True
@@ -33,7 +33,7 @@ BAD_DATA = {'nicknam3' : 'my paypal', 'user_id' : 2, 'payment_type' : 'paypal',
         
         data = CREDIT
 
-        payment = models.Payment()
+        payment = Payment()
         payment.deserialize(data)
         app_db.session.add(payment)
         app_db.session.commit()
@@ -43,21 +43,50 @@ BAD_DATA = {'nicknam3' : 'my paypal', 'user_id' : 2, 'payment_type' : 'paypal',
         app_db.session.remove()
         app_db.drop_all()
 
-    def test_payment_serialize(self):
-        pass
+    def test_payment_serialize_credit(self):
+        payment = Payment.query.get(1)
+        p = payment.serialize()
+        self.assertEqual(type(p), type({}))
+        self.assertEqual(p['nickname'], 'my credit')
+        self.assertEqual(p['user_id'], 1)
+        self.assertEqual(p['payment_type'], 'credit')
+        self.assertEqual(type(p['details']), type({}))
+    
+    def test_detail_serialize_credit(self):
+        detail = Detail.query.get(1)
+        d = detail.serialize()
+        self.assertEqual(d['user_name'], 'John Jameson')
+        self.assertEqual(d['card_number'], '4444333322221111')
+        self.assertEqual(d['card_type'], 'Visa')
+        self.assertEqual(d['expires'], '02/2020')
+        self.assertTrue('is_linked' not in d)
+        self.assertTrue('user_email' not in d)
 
+    def test_detail_serialize_paypal(self):
+        d = Detail()
+        d.deserialize_paypal(PAYPAL_DETAIL)
+        app_db.session.add(d)
+        app_db.session.commit()
+        detail = Detail.query.get(2)
+        d = detail.serialize()
+        self.assertEqual(d['user_name'], 'John Jameson')
+        self.assertEqual(d['user_email'], 'jj@aol.com')
+        self.assertEqual(d['is_linked'], True)
+        self.assertTrue('card_number' not in d)
+        self.assertTrue('card_type' not in d)
+        
     def test_payment_deserialize(self):
         pass
 
-    def test_db_has_one_item(self):
-        p1 = app_db.session.query(models.Payment).get(1)
-        self.assertNotEqual(p1, None)
-        p2 = app_db.session.query(models.Payment).get(2)
-        self.assertEqual(p2, None)
+    def test_detail_deserialize_credit(self):
+        pass
 
-    def test_credit_has_no_paypal_fields(self):
-        payment = app_db.session.query(models.Payment).get(1)
-        self.assertEqual(payment.nickname, 'my credit')
-        detail = payment.details
-        self.assertEqual(detail.is_linked, None)
-        self.assertEqual(detail.user_email, None)
+    def test_detail_deserialize_paypal(self):
+        pass
+
+    def test_payment_self_url(self):
+        payment = Payment.query.get(1)
+        with payments.app.test_request_context():
+            addr = payment.self_url()
+            self.assertTrue('/payments/1' in addr)
+        
