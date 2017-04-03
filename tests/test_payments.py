031d9df8bba996ddad281a1f8f2d1451d62733ad
odoric27 <dmo294@nyu.edu>
2017-04-02 20:44:16 -0400

    added error_handler tests. add __init__ to tests so we can 'coverage run' entire tests module

--- tests/test_payments.py
+++ tests/test_payments.py
@@ -2,11 +2,14 @@
 # python -m unittest discover
 # nosetests -v --rednose --nologcapture
 
-import unittest
+import unittest, json, mock
 
 from app import payments
 from app.db import app_db
-from app.db import models
+from app.error_handlers import DataValidationError
+from app.db.interface import PaymentService
+from app.db.models import Payment, Detail
+from flask_api import status    # HTTP Status Codes
 
 
 class TestModels(unittest.TestCase):
@@ -43,4 +46,65 @@ class TestModels(unittest.TestCase):
         detail = payment.details
         self.assertEqual(detail.is_linked, None)
         self.assertEqual(detail.user_email, None)
+        
+   def test_crud_put_not_allowed_for_create(self):
+        p = {'nickname' : 'my paypal', 'user_id' : 2, 'payment_type' : 'paypal', 
+        'details' : {'user_name' : 'John Jameson', 'user_email' : 'jj@aol.com'}}
+        data = json.dumps(p)
+        resp = self.app.put('/payments', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_crud_post_not_allowed_on_existing(self):
+        p = {"nickname" : "my credit", "user_id" : 1, "payment_type" : "credit",
+        "details" : {"user_name" : "Jimmy Jones", "card_number" : "1111222233334444",
+        "expires" : "01/2019", "card_type" : "Mastercard"}}
+        data = json.dumps(p)
+        resp = self.app.post('/payments/1', data=data, content_type='application/json')
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_crud_post_bad_data(self):
+        p = {"nicknam3" : "my credit", "user_id" : 1, "payment_type" : "credit",
+        "details" : {"user_name" : "Jimmy Jones", "card_number" : "1111222233334444",
+        "expires" : "01/2019", "card_type" : "Mastercard"}}
+        data = json.dumps(p)
+        resp = self.app.post('/payments', data=data, content_type='application/json')
+        self.assertEquals(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue('missing nickname' in resp.data)
+
+    def test_crud_post_no_data(self):
+        data = None
+        resp = self.app.post('/payments', data=data, content_type='application/json')
+        self.assertEquals(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue('bad or no data' in resp.data)
+
+    def test_crud_post_garbage(self):
+        garbage = 'coiNCEOI#$()&@#%&V$TC&nDFudfsf sdg g w9r w39 r70'
+        resp = self.app.post('/payments', data=garbage, content_type='application/json')
+        self.assertEquals(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTrue('bad or no data' in resp.data)
+
+    def test_crud_get_id_not_found(self):
+        resp = self.app.get('payments/777')
+        self.assertTrue(resp.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue('777 was not found' in resp.data)
+
+    def test_crud_put_id_not_found(self):
+        resp = self.app.put('payments/778')
+        self.assertTrue(resp.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue('778 was not found' in resp.data)
+
+    def test_crud_patch_id_not_found(self):
+        resp = self.app.put('payments/779')
+        self.assertTrue(resp.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue('779 was not found' in resp.data)
+
+    def test_crud_get_garbage1_not_found(self):
+        resp = self.app.get('payments/dflkdfsdflkjsd')
+        self.assertTrue(resp.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue('requested URL was not found' in resp.data)
+
+    def test_crud_get_garbage2_not_found(self):
+        resp = self.app.get('<24@#&*^@sd35vqr\]/.,diiv8989')
+        self.assertTrue(resp.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertTrue('requested URL was not found' in resp.data)
 
