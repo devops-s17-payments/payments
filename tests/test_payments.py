5410a1e2eac25c5fc71530e546a16b14b5fb271d
odoric27 <dmo294@nyu.edu>
2017-04-02 21:24:28 -0400

    refactor tests to put in their own class. pull out sample data from tests

--- tests/test_payments.py
+++ tests/test_payments.py
@@ -11,6 +11,12 @@ from app.db.interface import PaymentService
 from app.db.models import Payment, Detail
 from flask_api import status    # HTTP Status Codes
 
+CARD_DATA = {'nickname' : 'my credit', 'user_id' : 1, 'payment_type' : 'credit', 
+             'details' : {'user_name' : 'Jimmy Jones', 'card_number' : '1111222233334444',
+             'expires' : '01/2019', 'card_type' : 'Mastercard'}}
+
+PAYPAL_DATA = {'nickname' : 'my paypal', 'user_id' : 2, 'payment_type' : 'paypal', 
+               'details' : {'user_name' : 'John Jameson', 'user_email' : 'jj@aol.com'}}
 
 class TestModels(unittest.TestCase):
 
@@ -20,9 +26,7 @@ class TestModels(unittest.TestCase):
         app_db.drop_all()    # clean up the last tests
         app_db.create_all()  # make our sqlalchemy tables
         
-        data = {'nickname' : 'my credit', 'user_id' : 1, 'payment_type' : 'credit', 
-        'details' : {'user_name' : 'Jimmy Jones', 'card_number' : '1111222233334444',
-        'expires' : '01/2019', 'card_type' : 'Mastercard'}}
+        data = SAMPLE_DATA
 
         payment = Payment()
         payment.deserialize(data)
@@ -47,26 +51,23 @@ class TestModels(unittest.TestCase):
         self.assertEqual(detail.is_linked, None)
         self.assertEqual(detail.user_email, None)
 
+class TestModels(unittest.TestCase):
+
+    def setUp(self):
+        self.app = payments.app.test_client()
+        
     def test_crud_put_not_allowed_for_create(self):
-        p = {'nickname' : 'my paypal', 'user_id' : 2, 'payment_type' : 'paypal', 
-        'details' : {'user_name' : 'John Jameson', 'user_email' : 'jj@aol.com'}}
-        data = json.dumps(p)
+        data = json.dumps(PAYPAL_DATA)
         resp = self.app.put('/payments', data=data, content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_crud_post_not_allowed_on_existing(self):
-        p = {"nickname" : "my credit", "user_id" : 1, "payment_type" : "credit",
-        "details" : {"user_name" : "Jimmy Jones", "card_number" : "1111222233334444",
-        "expires" : "01/2019", "card_type" : "Mastercard"}}
-        data = json.dumps(p)
+        data = json.dumps(SAMPLE_DATA)
         resp = self.app.post('/payments/1', data=data, content_type='application/json')
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_crud_post_bad_data(self):
-        p = {"nicknam3" : "my credit", "user_id" : 1, "payment_type" : "credit",
-        "details" : {"user_name" : "Jimmy Jones", "card_number" : "1111222233334444",
-        "expires" : "01/2019", "card_type" : "Mastercard"}}
-        data = json.dumps(p)
+        data = json.dumps(SAMPLE_DATA)
         resp = self.app.post('/payments', data=data, content_type='application/json')
         self.assertEquals(resp.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertTrue('missing nickname' in resp.data)
