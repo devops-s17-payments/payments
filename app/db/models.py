e16643ddd088d16d0df8545198a38a41114b9acf
odoric27 <dmo294@nyu.edu>
2017-04-03 00:16:59 -0400

    finished tests for now. some code duplication from another branch was creating issues. resolved now.

--- app/db/models.py
+++ app/db/models.py
@@ -2,6 +2,8 @@
 from flask import url_for
 from app.db import app_db
 from app.error_handlers import DataValidationError
+
+
 ######################################################################
 # Models
 ######################################################################
