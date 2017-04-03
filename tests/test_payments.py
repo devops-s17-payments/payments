2f316f23be6e9f8a34cd8f9045d3f6a55f238d31
odoric27 <dmo294@nyu.edu>
2017-04-03 00:47:11 -0400

    refactored create(), started tests for same

--- tests/test_payments.py
+++ tests/test_payments.py
@@ -44,3 +44,40 @@ class TestModels(unittest.TestCase):
         self.assertEqual(detail.is_linked, None)
         self.assertEqual(detail.user_email, None)
 
+class TestPaymentsCRUD(unittest.TestCase):
+    """
+    Test cases for CRUD methods contained in payments.py.
+    """
+
+CREDIT = {'nickname' : 'my credit', 'user_id' : 1, 'payment_type' : 'credit', 
+          'details' : {'user_name' : 'Jimmy Jones', 'card_number' : '1111222233334444',
+          'expires' : '01/2019', 'card_type' : 'Mastercard'}}
+
+DEBIT = {'nickname' : 'my debit', 'user_id' : 2, 'payment_type' : 'debit', 
+         'details' : {'user_name' : 'John Jameson', 'card_number' : '4444333322221111',
+         'expires' : '02/2020', 'card_type' : 'Visa'}}
+
+PAYPAL = {'nickname' : 'my paypal', 'user_id' : 2, 'payment_type' : 'paypal', 
+          'details' : {'user_name' : 'John Jameson', 'user_email' : 'jj@aol.com'}}
+
+#note 'nickname' is spelled wrong
+BAD_DATA = {'nicknam3' : 'my paypal', 'user_id' : 2, 'payment_type' : 'paypal', 
+            'details' : {'user_name' : 'John Jameson', 'user_email' : 'jj@aol.com'}}
+
+    def setUp(self):
+        self.app = payments.app.test_client()
+
+    def test_crud_create_card(self):
+        with patch.object(PaymentService, 'add_payment', return_value=CREDIT) as mocked_service:
+            data = json.dumps(CREDIT)
+            resp = self.app.post('/payments', data=data, content_type='application/json')
+            self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+            self.assertEqual(json.loads(resp.data), {'created' : CREDIT})
+
+    def test_crud_create_paypal(self):
+        with patch.object(PaymentService, 'add_payment', return_value=DEBIT) as mocked_service:
+            data = json.dumps(DEBIT)
+            resp = self.app.post('/payments', data=data, content_type='application/json')
+            self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+            self.assertEqual(json.loads(resp.data), {'created' : DEBIT})
+
