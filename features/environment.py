fcaa540d0acad3837aa843469ecdf27e61b33bc9
odoric27 <dmo294@nyu.edu>
2017-04-06 13:16:33 -0400

    added new features/steps folders. config basic environ setup/teardown. added one 'home page' scenario just to make sure it works

---
+++ features/environment.py
@@ -0,0 +1,21 @@
+from behave import *
+from app import payments
+from app.db import app_db
+
+def before_all(context):
+
+    ''' 
+        this db stuff should go somewhere else
+        refactor will be another issue
+    '''
+    payments.app.debug = True
+    payments.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://payments:payments@localhost:5432/test'
+    app_db.drop_all()    # clean up the last tests
+    app_db.create_all()  # make our sqlalchemy tables
+    
+    context.app = payments.app.test_client()
+    context.server = payments
+
+def after_all(context):
+    app.db.drop_all()
+    context.server.shutdown()
