49a54fe308c72817143d19400a894310ed46ed06
odoric27 <dmo294@nyu.edu>
2017-04-06 13:16:33 -0400

    added new features/steps folders. config basic environ setup/teardown. added one 'home page' scenario just to make sure it works

---
+++ features/steps/payment_steps.py
@@ -0,0 +1,14 @@
+from behave import *
+import json
+
+@when(u'I visit the "home page"')
+def step_impl(context):
+    context.resp = context.app.get('/')
+
+@then(u'I should see "{message}"')
+def step_impl(context, message):
+    assert message in context.resp.data
+
+@then(u'I should not see "{message}"')
+def step_impl(context, message):
+    assert message not in context.resp.data
