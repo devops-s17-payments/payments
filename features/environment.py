b1e340df4345810c21f4614190f8a2e9da8e01ae
odoric27 <dmo294@nyu.edu>
2017-04-06 13:20:45 -0400

    quick fix to environment.py

--- features/environment.py
+++ features/environment.py
@@ -12,10 +12,10 @@ def before_all(context):
     payments.app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://payments:payments@localhost:5432/test'
     app_db.drop_all()    # clean up the last tests
     app_db.create_all()  # make our sqlalchemy tables
+    app_db.session.commit()
     
     context.app = payments.app.test_client()
     context.server = payments
 
 def after_all(context):
-    app.db.drop_all()
-    context.server.shutdown()
+    app_db.drop_all()
