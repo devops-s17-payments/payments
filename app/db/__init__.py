d8114ca9380353972fec3709a8d847159df9b674
odoric27 <dmo294@nyu.edu>
2017-04-06 05:10:55 +0000

    actually inits db and creates tables

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -7,3 +7,12 @@ db connection string current in config, but will implement a way to get from env
 '''
 
 app_db = SQLAlchemy(app)
+
+'''
+most import models AFTER db init
+but before table creation
+'''
+from app.db import models
+
+app_db.create_all()
+app_db.session.commit()
