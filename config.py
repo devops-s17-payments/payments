6809b843e2aa702d1d44270d918c79493b2c5f53
odoric27 <dmo294@nyu.edu>
2017-04-09 13:51:03 -0400

    removing instructions re: environ var export

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 import os, sys
 
-'''
-for local db, need to EXPORT env variable after vagrant ssh:
-export LOCAL_DB = postgresql://payments:payments@localhost:5432/dev
-'''
-
 if 'VCAP_SERVICES' in os.environ:
     VCAP_SERVICES = os.environ['VCAP_SERVICES']
     services = json.loads(VCAP_SERVICES)
