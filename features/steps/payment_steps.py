d4379950e2665606c9e63f53f194b71f80be057f
odoric27 <dmo294@nyu.edu>
2017-04-09 22:09:54 -0400

    removing unicode

--- features/steps/payment_steps.py
+++ features/steps/payment_steps.py
@@ -1,14 +1,14 @@
 from behave import given, when, then
 import json
 
-@when(u'I visit the "home page"')
+@when('I visit the "home page"')
 def step_impl(context):
     context.resp = context.app.get('/')
 
-@then(u'I should see "{message}"')
+@then('I should see "{message}"')
 def step_impl(context, message):
     assert message in context.resp.data
 
-@then(u'I should not see "{message}"')
+@then('I should not see "{message}"')
 def step_impl(context, message):
     assert message not in context.resp.data
