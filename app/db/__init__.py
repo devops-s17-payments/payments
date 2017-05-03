4f14b40531dc72d414ab65483f990acba76c462c
odoric27 <dmo294@nyu.edu>
2017-05-03 10:41:26 -0400

    added script to check if db is open before connecting

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -10,6 +10,6 @@ most import models AFTER db init
 but BEFORE table creation
 '''
 from app.db import models
-#app_db.drop_all()	# uncomment and run to wipe out dev db
+
 app_db.create_all()
 app_db.session.commit()
